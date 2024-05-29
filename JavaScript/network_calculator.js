9dc0330879a3e7f2a77da943a373c5ab8a3d2097
Corentin Lallement <corentinlallement5@gmail.com>
2024-05-29 16:04:23 +0200

    Fix: removed type annotations, they break everything

--- JavaScript/network_calculator.js
+++ JavaScript/network_calculator.js
@@ -1,12 +1,12 @@
-function ipToInt(ip : string) : number {
+function ipToInt(ip){
     return ip.split('.').reduce((int, octet) => (int << 8) + parseInt(octet, 10), 0) >>> 0;
 }
 
-function intToIp(int : number) : string {
+function intToIp(int) {
     return `${(int >>> 24)}.${(int >> 16 & 255)}.${(int >> 8 & 255)}.${(int & 255)}`;
 }
 
-function calculateNetworkInfo(previousBroadcast : number, groupSize : number) : [string, string, string, number, number] {
+function calculateNetworkInfo(previousBroadcast, groupSize){
     const newMask = 32 - Math.ceil(Math.log2(groupSize + 2));
     const addressAvailable = Math.pow(2, 32 - newMask);
     const networkAddressInt = previousBroadcast + 1;
@@ -18,13 +18,13 @@ function calculateNetworkInfo(previousBroadcast : number, groupSize : number) : 
     return [networkAddress, broadcastAddress, addressRange, addressAvailable, newMask];
 }
 
-function isNetworkAddress(ip : undefined, mask : number) : boolean {
+function isNetworkAddress(ip, mask) {
     const ipInt = ipToInt(ip);
     const maskInt = ~((1 << (32 - mask)) - 1);
     return (ipInt % maskInt) === 0;
 }
 
-function validateInputs() : boolean | undefined {
+function validateInputs() {
     try {
         const baseNetworkIP = document.getElementById('entryBaseNetworkIP').value;
         const baseNetworkMask = parseInt(document.getElementById('entryBaseNetworkMask').value);
@@ -47,7 +47,7 @@ function validateInputs() : boolean | undefined {
     }
 }
 
-function displayResults() : void {
+function displayResults()  {
     if (!validateInputs()) {
         return;
     }
